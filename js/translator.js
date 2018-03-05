7191455224205784ed119a0e5ea3ecaa4541119c
anna waluszko <ashiko85@gmail.com>
2018-03-05 13:53:15 +0100

    Add new languages with google translate

---
+++ js/translator.js
@@ -0,0 +1,3 @@
+function googleTranslateElementInit() {
+    new google.translate.TranslateElement({pageLanguage: 'pl', includedLanguages: 'en,ko,vi,de', layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element');
+}
