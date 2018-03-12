60a6e54b15e7d7ebaafc92023cf46c5de8248963
anna waluszko <ashiko85@gmail.com>
2018-03-12 22:10:00 +0100

    Adding file cookies.js and cookies info in index.html

---
+++ js/cookies.js
@@ -0,0 +1,14 @@
+function showNotice() {
+    if(localStorage.getItem('showCookieNotice') !== '') {
+        document.querySelector('#cookie-notice').classList.remove('hide');
+    }
+}
+
+function hideNotice() {
+    document.querySelector('#cookie-notice').classList.add('hide');
+    localStorage.setItem('showCookieNotice', 'false');
+}
+
+showNotice();
+
+document.querySelector( '#cookie-notice-button' ).addEventListener( 'click', hideNotice );
