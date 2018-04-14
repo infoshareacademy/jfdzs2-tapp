4b19b42ed6be2a345979571331910b03c476cf30
anna waluszko <ashiko85@gmail.com>
2018-04-14 12:56:52 +0200

    Changing functions names

--- js/scrolltotop.js
+++ js/scrolltotop.js
@@ -1,13 +1,11 @@
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
+window.onscroll = function onScroll() {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
         document.getElementById("to-top-button").style.display = "block";
     } else {
         document.getElementById("to-top-button").style.display = "none";
     }
-}
+};
 
-function topFunction() {
+function scrollToTop() {
     document.documentElement.scrollTop = 0;
 }
