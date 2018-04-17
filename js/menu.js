c8a49ce0b823c74e77cec52a86d105b9aad44c2e
Piotr Kowalczyk <contact@piotrkowalczyk.com>
2018-04-17 21:06:47 +0200

    fix to colors and outline of the index.html

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,5 @@
 
-$('body').scrollspy({ target: '#bs-example-navbar-collapse-1' });
+$('body').scrollspy({ target: '#bs-tapp-navbar-collapse-1' });
 $('[data-spy="scroll"]').each(function () {
     var $spy = $(this).scrollspy('refresh')
 });
