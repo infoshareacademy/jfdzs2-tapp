9eefd1df863d9c77c2dbd0a37ed8f33d521e039b
anna waluszko <ashiko85@gmail.com>
2018-06-27 16:17:12 +0200

    Reorganizing

--- js/load-game.js
+++
@@ -1,6 +0,0 @@
-//function for loading game, use dinamic loading jquery ajax method
-$(document).ready(function(){
-    $("#loading-game").click(function(){
-        $("#form").load("page1.html");
-    });
-});
